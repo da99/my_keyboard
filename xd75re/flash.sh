31132b038f81ee24816a6445a66f8eed0527e3a6
da99 <da@bdrm>
2019-05-21 17:41:13 -0500

    Development checkpoint.

--- xd75re/flash.sh
+++ xd75re/flash.sh
@@ -1,9 +1,14 @@
 
 set -x
 set -e
+the_file="/apps/my_keyboard/xd75re/xd75_da99.hex"
+stat --format "%s" "$the_file"
+sleep 1; echo -n ".";
+sleep 1; echo -n ".";
+sleep 1; echo -n ".";
 lsusb | grep "DFU bootloader"
 sudo dfu-programmer atmega32u4 erase --force
 sleep 2
-sudo dfu-programmer atmega32u4 flash xd75_da99.hex
+sudo dfu-programmer atmega32u4 flash "$the_file"
 sleep 2
 sudo dfu-programmer atmega32u4 reset
